7c03af137ca4204c155293259cdacbd4c3a56ab0
erretres <diegoperezcorreo@gmail.com>
2020-05-28 18:31:54 -0400

    Rewrite gulp task: generate a graph.png file.

--- examples/trafficlight/trafficlight.js
+++ examples/trafficlight/trafficlight.js
@@ -6,7 +6,7 @@ const actions     = require('./statecharts/actions.js');
 const options     = {outputType: "dot"};
 const diagram     = fs.readFileSync(path.resolve(__dirname, './statecharts/diagram.txt'),'utf8'); 
 
-const trafficLightMachine = statecharts(diagram,actions);
+const trafficLightMachine = statecharts(diagram,actions,options);
 const trafficLightService = interpret(trafficLightMachine).onTransition(state=> console.log(state.value));
 
 trafficLightService.start();
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
-var path          = require('path');
-var exec          = require("child_process").exec;
-var gulp          = require("gulp");
-var parallel      = gulp.parallel; 
+var path       = require('path');
+var exec       = require("child_process").exec;
+var gulp       = require("gulp");
+var parallel   = gulp.parallel; 
+var args       = process.argv;
+var watcher    = gulp.watch('./examples/**/statecharts/*.txt');
 
 
+function f(done) {
+	watcher.on('change', (path,stats) => {
+		let a = path.replace(/diagram.txt/,'');
+	  let b = a.replace(/statecharts\//, ''); 
+
+		exec(`cd ./${b}; node trafficlight.js; cd ./statecharts; dot graph.dot -Tpng -o graph.png; cd ../../../; bash refresh.sh; echo 'done'`, (err, stdout, stderr) => {
+				if(stderr) {
+						console.log(stderr);
+				}else{
+           console.log(stdout);
+				}
+
+    });	
+		});
+
+		done();
+}
 
 
-exports.start   = parallel();
+exports.start   = parallel(f);
 
 
 
