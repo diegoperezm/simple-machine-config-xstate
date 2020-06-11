f309337dc75f1387d26eade1c7c959319aa2df2c
erretres <diegoperezcorreo@gmail.com>
2020-06-11 18:11:51 -0400

    Remove from index.js: graph (export).

--- statecharts/index.js
+++ statecharts/index.js
@@ -1,11 +1,11 @@
 const  statecharts = require("./statecharts.js");
 const  machineConf = require("./machineConf.js");
 const  client      = require("./client.js");
-const  graph       = require("./graph.js");
+//const  graph       = require("./graph.js");
 
 
 exports.statecharts = statecharts;
 exports.machineConf = machineConf;
 exports.client      = client;
-exports.graph       = graph;
+//exports.graph       = graph;
 
