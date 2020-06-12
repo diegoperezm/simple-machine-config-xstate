e0fcec430c17640d2ec6df40f35d08cd15c8a2a5
erretres <diegoperezcorreo@gmail.com>
2020-06-12 14:15:32 -0400

    Check if feh is running.
    If feh is running: run refresh.sh
    If feh is not running: run feh then run refresh.sh

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,41 @@
-var path               = require('path');
-var exec               = require("child_process").exec;
-var gulp               = require("gulp");
-var parallel           = gulp.parallel; 
-var watcherDiagram     = gulp.watch('./examples/**/statecharts/*.txt');
 
+var path           = require('path');
+var exec           = require("child_process").exec;
+var gulp           = require("gulp");
+var parallel       = gulp.parallel; 
+var watcherDiagram = gulp.watch('./examples/**/statecharts/*.txt');
+var isFehRunning = false;
 function graph(done) {
+
   watcherDiagram.on('change', (path,stats) => {
     let a = path.replace(/diagram.txt/,'');
     let b = a.replace(/statecharts\//, ''); 
-		let commands = `cd ./${b};` +
-  			           'node ../../statecharts/graph.js ./statecharts/diagram.txt "dot";' +
-				           'cd ./statecharts;' +
-				           'dot graph.dot -Tpng -o graph.png;' +
-				           'cd ../../../;' +
-				           'bash refresh.sh';  
-			exec(commands, (err, stdout, stderr) => {
-					if(stderr) {
-							console.log(stderr);
-					} else {
-							console.log(stdout);
-					}
-			});
-	});
-		done();
+    let forFeh = '';
+
+    if(isFehRunning) {
+      forFeh ='cd ../../../; bash refresh.sh';
+    } else {
+      forFeh ='feh graph.png;' + 
+              'cd ../../../; bash refresh.sh';
+      isFehRunning = true;
+    }
+
+    let commands = `cd ./${b};` +
+    'node ../../statecharts/graph.js '   +
+    './statecharts/diagram.txt "dot";'  +
+    'cd ./statecharts;'                 +
+    'dot graph.dot -Tpng -o graph.png;' +
+    `${forFeh}`;  
+
+    exec(commands, (err, stdout, stderr) => {
+      if(stderr) {
+	console.log(stderr);
+      } else {
+	console.log(stdout);
+      }
+    });
+   });
+  done();
 }
 
 
