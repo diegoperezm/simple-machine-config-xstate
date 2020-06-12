15aa508f424960545c758562ce0a9943eefe9d7b
erretres <diegoperezcorreo@gmail.com>
2020-06-12 10:42:31 -0400

    Add comments to: machineConf

--- statecharts/machineConf.js
+++ statecharts/machineConf.js
@@ -3,16 +3,26 @@ const fs          = require('fs');
 const path        = require('path');
 const jison       = require('jison');
 const bnf         = fs.readFileSync(
-                        path.resolve(__dirname,'./grammar.jison'),
+                        path.resolve(
+                        __dirname,
+                        './grammar.jison'),
                         'utf8');
 const parser      = new jison.Parser(bnf);
 const execParser  = input => parser.parse(input); 
 
+/**
+ * @param   {string} diagram - A statechart description       
+ * @example 
+ * let diagram = `*A time B
+ *                 B final`
+ * machineConf(diagram);
+ * // => {"initial":"A","context":{},"states":{"A":{"on":{"time":{"target":"B"}}},"B":{"type":"final"}}} 
+ * @returns {object}         - A machine conf (xstate) 
+ */
 const machineConf  = (diagram) =>  {
   const machine      = execParser(diagram);
   return machine;
 };
-
 
 
 module.exports = machineConf;
