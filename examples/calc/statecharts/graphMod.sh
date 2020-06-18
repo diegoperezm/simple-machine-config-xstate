b1ab9ac760ec2563fb750a55a6ddd20e9d9e67c6
erretres <diegoperezcorreo@gmail.com>
2020-06-18 15:31:57 -0400

    Modify diagram: names and script to look better.

--- examples/calc/statecharts/graphMod.sh
+++ examples/calc/statecharts/graphMod.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sed -i '2s/$/ rankdir=LR /' graph.dot;
+sed -i 's/label=" "/weight=20  label=" "/' graph.dot;
 sed -i 's/label="dot/fontcolor=orange color=orange label="dot/' graph.dot;
 sed -i 's/label="zero/fontcolor=purple color=purple label="zero/' graph.dot;
 sed -i 's/label="operator/fontcolor=green color=green label="operator/' graph.dot;
-sed -i 's/label="number/fontcolor=red color=red label="number/' graph.dot;
-#sed -i 's/label="ce/color=white  label="ce' graph.dot;
- 
+sed -i 's/label="number/fontcolor=grey color=grey label="number/' graph.dot;
+sed -i 's/label="ce/fontcolor=black color=black label="ce/' graph.dot;
+sed -i 's/label="minus/fontcolor=red color=red label="minus/' graph.dot;
+sed -i '67s/fontcolor=red/weight=20 fontcolor=red/' graph.dot;
