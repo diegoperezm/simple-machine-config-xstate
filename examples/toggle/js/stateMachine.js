9e8ad4abedfc8b515085f990d4b1f2d1244d9d0c
erretres <diegoperezcorreo@gmail.com>
2020-06-27 10:27:31 -0400

    Remove examples.

--- examples/toggle/js/stateMachine.js
+++
@@ -1,1 +0,0 @@
-const toggleConf = {"initial":"INACTIVE","context":{},"states":{"INACTIVE":{"on":{"toggle":{"target":"ACTIVE","actions":["classon"]}}},"ACTIVE":{"on":{"toggle":{"target":"INACTIVE","actions":["classoff"]}}}}};
