1ab8fc2f480b350907199a416bcd1ca973ad3233
erretres <diegoperezcorreo@gmail.com>
2020-07-04 09:25:38 -0400

    Add test: extended state (context) with number.

---
+++ test/xspTrafficlightsExtendedStateWithNumber.js
@@ -0,0 +1,61 @@
+const test   = require('../utils/utils.js');
+const xstateParse  = require('../statecharts/grammarXstate.js').parse;
+
+/**
+ *             ABBREVIATIONS
+ *
+ *   stt<>     = state  transition  table
+ *   xsp<>     = xstate parsed      machine conf
+ *
+ */
+
+
+  const sttTrafficlightsExtendedStateWithNumber =
+`
+ context:
+   color: 0 
+
+   *GREEN   time YELLOW
+    YELLOW  time RED
+    RED     time GREEN
+`;
+
+  const xspTrafficlightsExtendedStateWithNumber = xstateParse(sttTrafficlightsExtendedStateWithNumber);
+
+   const  trafficlightsExtendedStateWithNumberObject = {
+      "initial": "GREEN",
+        "context": {
+            "color": 0 
+        },
+      "states": {
+        "GREEN": {
+          "on": {
+            "time": {
+              "target": "YELLOW",
+            }
+          }
+        },
+        "YELLOW": {
+          "on": {
+            "time": {
+              "target": "RED",
+           }
+          }
+        },
+        "RED": {
+          "on": {
+            "time": {
+              "target": "GREEN",
+            }
+          }
+        }
+      }
+    };
+
+
+
+
+test('xspTrafficlightsExtendedStateWithNumber and trafficlightsExtendedStateWithNumberObject',
+      xspTrafficlightsExtendedStateWithNumber,
+      trafficlightsExtendedStateWithNumberObject);
+
