fef773bd8f67332b37f825eb390f90fbb2f5dc47
erretres <diegoperezcorreo@gmail.com>
2020-07-07 09:00:31 -0400

    Add test for: invoke.

---
+++ test/xspSimpleInvoke.js
@@ -0,0 +1,60 @@
+const test   = require('../utils/utils.js');
+const xstateParse  = require('../statecharts/grammarXstate.js').parse;
+
+/**
+ *             ABBREVIATIONS
+ *   
+ *   stt<>     = state  transition  table
+ *   xsp<>     = xstate parsed      machine conf
+ *
+ */
+
+  const sttSimpleInvoke = 
+  `
+ invoke:
+     id: someid
+    src: somefunc
+ ondone: SUCCESS
+onerror: ERROR
+
+  *A @someid
+   B time SUCCESS
+  `;
+
+  const xspSimpleInvoke = xstateParse(sttSimpleInvoke);
+  
+  const simpleInvokeObject =
+{
+  "initial": "A",
+  "context": {},
+  "states": {
+    "A": {
+      "invoke": {
+        "id": "someid",
+        "src": "somefunc",
+        "onDone": {
+          "target": "SUCCESS"
+        },
+        "onError": {
+          "target": "ERROR"
+        }
+      }
+    },
+			"B": {
+							"on": {
+											"time": {
+															target: "SUCCESS"
+											}
+							}
+			}
+  }
+};
+
+//console.log(JSON.stringify(xspSimpleInvoke,null,2));
+test(
+  'xspSimpleInvoke and simpleInvokeObject',
+  xspSimpleInvoke,
+  simpleInvokeObject);
+
+
+
