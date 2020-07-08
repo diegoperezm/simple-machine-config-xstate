f76fcf4d21710b89402048dc60dde47d9105190d
erretres <diegoperezcorreo@gmail.com>
2020-07-08 06:47:56 -0400

    Add a test for xstate parser.
     - xstate parser convert To F invoke with actions

---
+++ test/xspConvertToFCtxInvokeWithActions.js
@@ -0,0 +1,116 @@
+const test   = require('../utils/utils.js');
+const xstateParse  = require('../statecharts/grammarXstate.js').parse;
+
+/**
+ *             ABBREVIATIONS
+ *   
+ *   stt<>     = state  transition  table
+ *   xsp<>     = xstate parsed      machine conf
+ *
+ */
+
+const sttConvertToFCtxInvokeWithActions = 
+`
+context:
+  celsius: ""
+  result:  "" 
+
+ invoke:
+     id: getinput
+    src: getinput
+ ondone: CALCF            :setctxcelsius
+onerror: ERROR
+
+ invoke:
+     id: calcf
+    src: calcf 
+ ondone: DISPLAYRESULT     :setctxresult
+onerror: ERROR
+
+ invoke:
+     id: displayresult 
+    src: displayresult
+ ondone: IDLE              :setctxcelsiusstr :setctxresultstr
+onerror: ERROR
+
+
+*IDLE          calculate        READINPUT
+
+ READINPUT     @getinput       
+
+ CALCF         @calcf        
+
+ DISPLAYRESULT @displayresult   
+ 
+ ERROR 
+`;
+
+const xspConvertToFCtxInvokeWithActions = xstateParse(sttConvertToFCtxInvokeWithActions);
+  
+const ConvertToFCtxInvokeWithActionsObject = {
+    "initial": "IDLE",
+    "context": {
+          "celsius": "",
+          "result":  ""
+    },
+    "states": {
+      "IDLE": {
+        "on": {
+            "calculate": {
+              "target": "READINPUT",
+            }
+          } 
+        },
+       "READINPUT": {
+         "invoke": {
+           "id": "getinput",
+           "src":"getinput",
+           "onDone": {
+             "target": "CALCF",
+             "actions": ["setctxcelsius"] 
+           },
+           "onError": {
+             "target": "ERROR"
+           }
+         }
+       },
+       "CALCF": {
+         "invoke": {
+           "id": "calcf",
+           "src": "calcf",
+           "onDone": {
+             "target": "DISPLAYRESULT",
+             "actions": ["setctxresult"] 
+           },
+           "onError": {
+             "target": "ERROR"
+           }
+         }
+        },
+      "ERROR":           {},
+      "DISPLAYRESULT":   {
+          "invoke": {
+           "id": "displayresult",
+           "src":"displayresult",
+           "onDone": {
+             "target": "IDLE",
+               "actions": ["setctxcelsiusstr",
+                           "setctxresultstr"] 
+           },
+           "onError": {
+             "target": "ERROR"
+           }
+         }
+        },
+      }
+    };
+
+
+test(
+  'xspConvertToFCtxInvokeWithActions and ConvertToFCtxInvokeWithActionsObject',
+  xspConvertToFCtxInvokeWithActions,
+  ConvertToFCtxInvokeWithActionsObject);
+
+
+//console.log(JSON.stringify(xspConvertToFCtxInvokeWithActions,null,2));
+
