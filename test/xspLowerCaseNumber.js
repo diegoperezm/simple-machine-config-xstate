666ac2ad30e65e3abf61c7baa02c57f2390ff66a
erretres <diegoperezcorreo@gmail.com>
2020-07-11 08:25:21 -0400

    Add test for: xsparser LOWERCASE accept digit

---
+++ test/xspLowerCaseNumber.js
@@ -0,0 +1,54 @@
+const test         = require('../utils/utils.js');
+const xstateParse  = require('../statecharts/grammarXstate.js').parse;
+
+/**
+ *             ABBREVIATIONS
+ *
+ *   stt<>     = state  transition  table
+ *   xsp<>     = xstate parsed      machine conf
+ *
+ */
+
+
+  const sttLowerCaseNumber =
+`
+context:
+  opnd1: [0]
+
+*A time B
+ B final
+`;
+
+ const xspLowerCaseNumber = xstateParse(sttLowerCaseNumber);
+
+ const  lowerCaseNumberObject = {
+
+  "initial": "A",
+  "context": {
+    "opnd1": [
+      0
+    ]
+  },
+  "states": {
+    "A": {
+      "on": {
+        "time": {
+          "target": "B"
+        }
+      }
+    },
+    "B": {
+      "type": "final"
+    }
+  }
+};
+       
+
+
+test('xspLowerCaseNumber and lowerCaseNumberObject',
+      xspLowerCaseNumber,
+      lowerCaseNumberObject);
+
+
+
+
