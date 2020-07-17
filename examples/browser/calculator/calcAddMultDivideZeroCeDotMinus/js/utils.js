d68950b2800332ec01d20b2bb1af0feb18153fd9
erretres <diegoperezcorreo@gmail.com>
2020-07-17 10:38:56 -0400

    Add example: calc add mult div zero ce dot minus.
    Use  the js folder.

---
+++ examples/browser/calculator/calcAddMultDivideZeroCeDotMinus/js/utils.js
@@ -0,0 +1,82 @@
+/**
+ * This is a simple/naive implementation, first draft etc.:
+ *
+ *  is not tested (only "works")
+ * 
+ */
+function highlightCurrentPath(state,prevEdges) {
+    let tspanElements = document.getElementsByTagName('tspan');
+    let tspanArr      = Array.from(tspanElements);  
+    let a;
+    let input;
+    let v;
+    let w;
+    let name;
+    let labelStyle;
+    
+   if(state.event.type=== 'xstate.init') {
+       input= state.event.type; 
+       v    = 'INITIAL';
+       w    = state.value;
+       name = (v+"-"+w+"-"+state.event.type);
+       window.g.node(w).elem.style = "fill: red; ";
+       a = window.g.edge(v,w,name).elem;
+       a.children[0].setAttribute('class', 'edgePath active');
+       prevEdges.push([v,w,name]);
+   }
+   if(state.changed) {
+      v    = prevEdges[0][0];
+      w    = prevEdges[0][1];
+      name = prevEdges[0][2]; 
+      a = window.g.edge(v,w,name).elem;
+      a.children[0].setAttribute('class', 'edgePath');
+      window.g.node(w).elem.style = "fill: black";
+
+      labelStyle = tspanArr.filter( elem => elem.__data__.name === name); 
+      labelStyle[0].classList.remove('active');
+
+      v    = w; 
+      w    = state.value;
+      name = (v+"-"+w+"-"+state.event.type);
+      window.g.node(w).elem.style = "fill: red; ";
+      a = window.g.edge(v,w, name).elem;
+      a.children[0].setAttribute('class', 'edgePath active');
+
+      labelStyle = tspanArr.filter( elem => elem.__data__.name === name); 
+      labelStyle[0].classList.add('active');
+
+      prevEdges.shift();
+      prevEdges.push([v,w,name]);
+
+   }
+}
+
+
+function graphSetStatesEdges(arr) {
+ let states = arr[0];
+ let edges  = arr[1];
+  
+states.forEach(function(state) {
+   if(state === "FINAL" || state === 'INITIAL') {
+     g.setNode(state, { label: "" }); 
+     g.node(state).style = "fill: #333";
+   } else {
+     g.setNode(state, { label: state }); 
+   }
+   });
+
+   edges.forEach(
+     function(ele) {
+       g.setEdge(
+         {v: ele[0],
+          w: ele[1],
+          name: ele[3]},
+         {label: ele[2]});});
+
+   // Set some general styles
+   g.nodes().forEach(function(v) {
+     var node = g.node(v);
+     node.rx = node.ry = 10;
+   });
+}
+
